c1c8d2d0c2373b21e5ec336da74598173eaef8a8
magocod <magoyeison@gmail.com>
2022-02-21 20:30:13 -0400

    update submodule call

--- src/controllers/submodule.controller.ts
+++ src/controllers/submodule.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import { GenericResponse } from "../interfaces";
 
-import { counter } from '../../../ts_module/src'
+import * as tsModule from '../../../ts_module/src'
 
 export async function count(req: Request, res: Response): GenericResponse {
     try {
-        return res.status(200).json(counter());
+        return res.status(200).json(tsModule.counter());
     } catch (err) {
         let message = "error cargando";
         if (err instanceof Error) {
