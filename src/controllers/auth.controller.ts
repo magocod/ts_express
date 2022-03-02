afa27cc0a9bd0d63e560523e63744d3a9c6739d6
magocod <magoyeison@gmail.com>
2022-03-01 20:30:21 -0400

    update dependencies

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 
+import { GenericResponse } from "../interfaces";
+
 export async function signup(req: Request, res: Response): Promise<void> {
   console.log("Register User");
   res.send("Register User");
 }
 
-export async function login(req: Request, res: Response): Promise<void> {
+export async function login(req: Request, res: Response): GenericResponse {
   console.log("Login");
-  res.send("Login");
+  return res.json({  message: 'message' });
 }
