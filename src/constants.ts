6ab48e5cc331ac55f3f79e7ead4a891da2a66440
magocod <magoyeison@gmail.com>
2022-09-25 22:22:13 -0400

    require cache

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,6 @@ import path from "path";
 export const tmpPath = path.resolve(__dirname, "../tmp");
 
 export const exampleTmpPath = path.join(tmpPath, "example");
+
+// testing require cache
+export const counterServicePath = path.resolve(__dirname, "./services/counter.service.ts");
--- test/counter/counter_service.spec.ts
+++ test/counter/counter_service.spec.ts
@@ -1,10 +1,12 @@
 import { assert } from "chai";
 import { counterService } from "../../src/services";
+import { counterServicePath } from "../../src/constants"
 
 describe("counter_service", function () {
   it("instance", function () {
     const initCount = counterService.getCount();
     const count = counterService.increment();
+    // console.log(require.cache[counterServicePath])
 
     // console.log("initCount", initCount, "count", count)
     assert.equal(count, initCount + 1);
